9af25a861e72d4899edd37c036159ce7824af89a
github username <github email address>
2020-01-31 17:59:47 -0800

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,53 +1,54 @@
 require 'pry'
 class PigLatinizer
+end
 
-    attr_reader :string
+    # attr_reader :string
 
-    def piglatinize(string)
-        @string = string
-        string = string.downcase
+    # def piglatinize(string)
+    #     @string = string.downcase
         
-        if starts_with_vowel
-            "#{string}way"
-        # elsif first_two_letters_consonsants
-        #     "#{string[2..-1]}#{string[0..1]}ay"
-        elsif !starts_with_vowel # starts with consonant
-            "#{string[1..-1]}#{string[0]}ay"
-        end
-    end
+    #     if starts_with_vowel
+    #         "#{@string}way"
+    #     # elsif first_two_letters_consonsants
+    #     #     "#{string[2..-1]}#{string[0..1]}ay"
+    #     elsif !starts_with_vowel # starts with consonant
+    #         "#{@string[1..-1]}#{@string[0]}ay"
+    #     end
+    # end
+
+
 
     # def find_of_vowels
     #     string.scan(/[aeoui]/)
     # end
 
-    def starts_with_vowel
-        if string[0] == 'A' || string[0] == 'E' || string[0] == 'I' || string[0] == 'O' || string[0] == 'U'
-            true
-        else
-            false
-        end
-    end
+    # def starts_with_vowel
+    #     if @string[0] == 'a' || @string[0] == 'e' || @string[0] == 'i' || @string[0] == 'o' || @string[0] == 'u'
+    #         true
+    #     else
+    #         false
+    #     end
+    # end
 
-    def first_two_letters_consonsants
-        binding.pry
-        if !starts_with_vowel && string[1] != 'a' || string[1] != 'e' || string[1] != 'i' || string[1] != 'o' || string[1] != 'u'
-            true 
-        else
-            false
-        end
-    end
+    # def first_two_letters_consonsants
+    #     # binding.pry
+    #     if !starts_with_vowel && @string[1] != 'a' || @string[1] != 'e' || @string[1] != 'i' || @string[1] != 'o' || @string[1] != 'u'
+    #         true 
+    #     else
+    #         false
+    #     end
+    # end
 
-    def first_three_letters_consonsants
-        # binding.pry
-        # if !starts_with_vowel && string[1] != 'a' || string[1] != 'e' || string[1] != 'i' || string[1] != 'o' || string[1] != 'u'
-        #     true 
-        # else
-        #     false
-        # end
-    end
+    # def first_three_letters_consonsants
+    #     # binding.pry
+    #     # if !starts_with_vowel && string[1] != 'a' || string[1] != 'e' || string[1] != 'i' || string[1] != 'o' || string[1] != 'u'
+    #     #     true 
+    #     # else
+    #     #     false
+    #     # end
+    # end
 
-    def split_up_string
+    # def split_up_string
+    #     # string.
 
-    end
-
-end
+    # end
