d0e842a66480f1f8ae1a32b12119975d3e484971
nwscala <nwscala@gmail.com>
2020-02-04 14:18:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        pig_latinizer = PigLatinizer.new
+        @pig_latin = pig_latinizer.piglatinize(params[:user_phrase])
+        erb :pig_latin_results
+    end
 end
