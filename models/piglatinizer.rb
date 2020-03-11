02a4007bada2cd47a66d5411f0ae218780abc8a9
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-03-11 22:59:30 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,6 @@
 class PigLatinizer
 
-  def piglatinize(user_phrase)
+  def pig_latinize(user_phrase)
     @new_word = user_phrase.split("")
     vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
     if vowels.include?(@new_word[0])
@@ -11,11 +11,11 @@ class PigLatinizer
     latinized_word
   end
 
-  def to_pig_latin(sentence)
+  def piglatinize(sentence)
     words_in_sentence = sentence.split(" ")
 
     pig_latinized_words = words_in_sentence.collect do |word|
-      self.piglatinize(word)
+      self.pig_latinize(word)
     end
 
     new_sentence = pig_latinized_words.join(" ")
